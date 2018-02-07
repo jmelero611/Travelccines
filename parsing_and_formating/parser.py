289315972cb969391488ddc5eabd8de0ec4f1f6d
AlvaroSmorras <35376500+AlvaroSmorras@users.noreply.github.com>
2018-02-07 13:08:19 +0100

    Update parser.py

--- parsing_and_formating/parser.py
+++ parsing_and_formating/parser.py
@@ -220,13 +220,25 @@ def save_tables(dhasc = False, country_table = False, c_has_rec = False, disease
             for i in range(len(trans_table)):
                 t.write('%s\t%s\n' %(i, trans_table[i]))
     if c_in_disease:
+        union = set()
         with open('tables/c_in_disease.tbl', "w") as cid:
             for dis in c_in_disease:
                 cid.write('%s: %s\n' %(dis, c_in_disease[dis]))
+                union = union.union( c_in_disease[dis])
+            cid.write('Union\t%s\n' %union)
 
+def dict_three_to_num():
+    ttn = {}
+    with open('countris_iso_table.tbl', 'r') as t_to_numm:
+        for line in t_to_numm:
+            line = line.strip()
+            line = line.split('\t')
+            ttn[line[2][:-1]] = int(line[3])
+    return ttn
 
 def html_to_table(list_of_countries):
     name_to_three = dict_name_to_three()
+    three_to_num = dict_three_to_num()
     d_has_c = []
     country_table = []
     c_has_rec = []
@@ -241,7 +253,8 @@ def html_to_table(list_of_countries):
         for disease in c_vaccinable:
             d_has_c.append((disease, name_to_three[country]))
             diseases_table[disease] = ('patogen', 'importance', 'yes')
-            countries_in_disease.setdefault(disease, set()).add(country)
+            if name_to_three[country] in three_to_num:
+                countries_in_disease.setdefault(disease, set()).add(three_to_num[name_to_three[country]])
             for transmission in c_vaccinable[disease]['transmission']:
                 if transmission != '':
                     if transmission not in trans_table:
@@ -249,7 +262,9 @@ def html_to_table(list_of_countries):
                     d_has_t.add((disease, trans_table.index(transmission)))
         for other_disease in c_other_diseases:
             diseases_table[other_disease] = ('patogen', 'importance', 'no')
-            countries_in_disease.setdefault(disease, set).add(country)
+            d_has_c.append((other_disease, name_to_three[country]))
+            if name_to_three[country] in three_to_num:
+                countries_in_disease.setdefault(other_disease, set()).add(three_to_num[name_to_three[country]])
         for r_typ in c_patient_counseling:
             for recomendation in c_patient_counseling[r_typ]:
                 if recomendation not in c_rec :
@@ -265,188 +280,6 @@ def html_to_table(list_of_countries):
                 d_has_t = d_has_t, c_rec_table = c_rec, trans_table = trans_table, c_in_disease = countries_in_disease)
 
 
-with open('cdc_paises.txt') as infile:
-    list = infile.read().split('\n')
-    html_to_table(list)
-
-    for line in url_to_html(file):
-        line.rstrip()
-        if re.search(r"\"vaccines-and-medicines\"",line):
-            vam_flag = True
-            continue
-
-        elif vam_flag:
-            if not tbody_flag:
-                if re.search(r"<tbody>",line):
-                    tbody_flag = True
-                    continue
-            else:
-                if clinical_flag:
-                    match = re.search(r'>(.*)</a>', line)
-                    if match:
-                        current_disease = match.group(1)
-                        vaccinable[current_disease] = {"recommendations":"",
-                                                       "transmission":""}
-                        clinical_flag = False
-                        continue
-                elif re.search(r'class="clinician-disease"', line):
-                    clinical_flag = True
-                    continue
-
-                elif re.search(r'class="clinician-recomendations"', line):
-                    recommendations_flag = True
-                    continue
-
-                elif recommendations_flag:
-
-                    if not re.search(r'</td>', line):
-                        if not re.search(r'class=".*', line):
-                            line = remove_flags(line)
-                            vaccinable[current_disease]["recommendations"] += line
-
-                    else:
-                        recommendations_flag = False
-
-                elif re.search(r'class="clinician-transmission"', line):
-                    transmission_flag = True
-                    match = re.search(r'<p>(.*)',line).group(1)
-                    match = remove_flags(match)
-                    vaccinable[current_disease]["transmission"] = [match]
-                    continue
-
-                elif transmission_flag:
-                    if not re.search(r'</td>',line):
-                        if not re.search(r'class=".*',line):
-                            line = remove_flags(line)
-                            vaccinable[current_disease]["transmission"].append(line)
-
-                    else:
-                        transmission_flag = False
-
-                elif re.search(r'</tbody>',line):
-                    tbody_flag = False
-                    vam_flag = False
-
-        elif re.search(r'^\s+Patient Counseling',line):
-            counseling_flag = True
-            continue
-
-        elif re.search(r'Healthy Travel Packing List',line):
-            counseling_flag = False
-            continue
-
-        elif counseling_flag:
-            match_name = re.search(r'class="no-image">(.*)<',line)
-            if match_name:
-                counseling_new = False
-                current_counseling = match_name.group(1)
-                patient_counseling[current_counseling] = ""
-                continue
-
-            elif re.search(r'class="sub_section_body inner-content inner-content-collapsable"',line):
-                counseling_new = True
-
-            elif counseling_new:
-                if re.search(r'href="javascript:void(0);">hide', line) or re.search(r'Additional Resources', line):
-                    counseling_new = False
-                else:
-                    line = remove_flags(line)
-                    patient_counseling[current_counseling] += line
-
-        elif re.search(r'<h3>Non-Vaccine-Preventable Diseases',line):
-            non_vaccine_flag = True
-            continue
-
-        elif non_vaccine_flag:
-            match_name = re.search(r'<td class="other-clinician-disease">(.*)</td>',line)
-            if match_name:
-                other_disease_name = match_name.group(1)
-                other_diseases[other_disease_name] = ""
-                new_other_flag = True
-                continue
-            elif new_other_flag and re.search(r'<td class="other-clinician-notes">',line):
-                other_body_flag = True
-
-            elif other_body_flag:
-                if not re.search(r'</td>', line):
-                    line = remove_flags(line)
-                    other_diseases[other_disease_name] += line
-                else:
-                    new_other_flag = False
-                    other_body_flag = False
-                    continue
-            elif re.search(r'<a href="#" class="tp-link-policy">Top</a>',line):
-                non_vaccine_flag = False
-    return vaccinable ,patient_counseling ,other_diseases
-def dict_name_to_three():
-    name_to_three = {}
-    with open('three_to_country.txt', 'r') as c_t_3:
-        for line in c_t_3:
-            line = line.strip()
-            line = line.split('\t')
-            name_to_three[line[1].lower()] = line[0]
-    return name_to_three
-def get_maps(country):
-    '''
-    :param country:
-    :obs ojo que no ha funcionado con todos los paises!!
-    '''
-    chdir('maps')
-    system('wget https://wwwnc.cdc.gov/travel/images/map-%s.png' %country)
-    chdir('..')
-def save_tables(dhasc = False, country_table = False, c_has_rec = False, disease_table = False, d_has_t = False):
-    if not path.isdir('tables'):
-        mkdir('tables')
-    if dhasc:
-        with open('tables/disease_has_country.tbl', "w") as dc:
-            dc.write('disease\tidCountry\n')
-            for dc_pair in dhasc:
-                dc.write('%s\t%s\n' %(dc_pair[0], dc_pair[1]))
-    if country_table:
-        with open('tables/country.tbl', "w") as c:
-            c.write('idCountry\tCountry_name\tmap\tDevelopment\n')
-            for country in country_table:
-                c.write('%s\t%s\t%s\t%s\n' %(country[0], country[1], country[2], country[3]))
-    if c_has_rec:
-        with open('tables/country_has_recomendations.tbl', "w") as cr:
-            cr.write('idCountry\tRecomendations\n')
-            for cr_pair in c_has_rec:
-                cr.write('%s\t%s\n' %(cr_pair[0], cr_pair[1]))
-    if disease_table:
-        with open('tables/disease.tbl', "w") as d:
-            d.write('idDisease\tDisease_name\tPatogen\tImportance\n')
-            for disease in disease_table:
-                d.write('%s\t%s\t%s\t%s\n' %(disease_table[disease][0], disease, disease_table[disease][1], disease_table[disease][2]))
-    if d_has_t:
-        with open('tables/disease_has_transmission.tbl', "w") as dt:
-            dt.write('idDisease\tidTransmission\n')
-            for dt_pair in d_has_t:
-                dt.write('%s\t%s\n' %(dt_pair[0], dt_pair[1]))
-
-def html_to_table(list_of_countries):
-    name_to_three = dict_name_to_three()
-    d_has_c = []
-    country_table = []
-    c_has_rec = []
-    diseases_table = {}
-    d_has_t = set()
-    for country in list_of_countries:
-        print('Working on %s...' %country)
-        c_vaccinable, c_patient_counseling, c_other_diseases = parse_html('https://wwwnc.cdc.gov/travel/destinations/clinician/none/%s' %country)
-        for disease in c_vaccinable:
-            d_has_c.append((disease, name_to_three[country]))
-            diseases_table[disease] = ('id', 'patogen', 'importance')
-            for transmission in c_vaccinable[disease]['transmission']:
-                d_has_t.add((disease, transmission))
-        for recomendation in c_patient_counseling:
-            c_has_rec.append((recomendation, name_to_three[country]))
-        #get_maps(country)
-        country_table.append((name_to_three[country], country, 'maps/map-%s' %country, 1))
-        time.sleep(6)
-
-    save_tables(dhasc = d_has_c, country_table = country_table, c_has_rec = c_has_rec, disease_table = diseases_table, d_has_t = d_has_t)
-
-
-with open('cdc_paises.txt') as infile:
-    list = infile.read().split('\n')
+with open('cdc_paises.txt') as cdc:
+    list = cdc.read().split('\n')
     html_to_table(list)
