cd08843617d219420af26d240b1305cf37abbd1f
jmelero611 <33523450+jmelero611@users.noreply.github.com>
2018-02-26 22:09:15 +0100

    Add files via upload

--- load_data_with_header.sql
+++ load_data_with_header.sql
@@ -5,6 +5,50 @@
 
 use Traveccines; #si teniu un altre nom per la base de dades, fer servir el nom que tingueu.
 
-LOAD DATA LOCAL INFILE '/path/table.tbl'
-	INTO TABLE table_name
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/country_sin_comillas.tbl'
+	INTO TABLE Country
 	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/diseases.tbl'
+	INTO TABLE Diseases
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/seffect.tbl'
+	INTO TABLE S_effect
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/vaccines.tbl'
+	INTO TABLE Vaccines
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/transmission.tbl'
+	INTO TABLE Transmission
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/wiki.tbl'
+	INTO TABLE Wiki
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/country_has_diseases.tbl'
+	INTO TABLE Country_has_Diseases
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/disease_has_transmission.tbl'
+	INTO TABLE Diseases_has_Transmission
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+LOAD DATA LOCAL INFILE '/home/user/Dropbox/Apunts_master/DBW/project/tables/vaccines_has_seffect.tbl'
+	INTO TABLE Vaccines_has_S_effect
+	IGNORE 1 LINES; #la capçalera. Si no en té, elimineu o comenteu aquesta línia
+
+
+
+
+
